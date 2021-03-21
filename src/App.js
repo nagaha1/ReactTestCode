3b92db26fd00c07ef8f0da0376bb7419be184288
nagaha1 <nagaha1221@gmail.com>
2021-03-21 21:05:03 +0900

    コミット忘れ？

--- src/App.js
+++ src/App.js
@@ -1,37 +1,67 @@
-import React,{ useState } from 'react';
+import React, { useState } from "react";
 
 const App = props => {
-  const [name,setName] = useState(props.name);
-  const [price,setPrice] = useState(props.price);
+  // const [name, setName] = useState(props);
+  // setNameが２つ宣言されているのがおかしいと言っています
+  const [state, setState] = useState(props);
+  // stateを個々に渡していたが、まとめてオブジェクトを渡せます
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
   //このようにしてまとめているのね
   const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
-  }
+    // setPrice(props.price);
+    // setName(props.name);
+    //そもそも表示されているやつが違うところを指している
+    setState(props);
+  };
 
   return (
     <>
-      <p>現在の{name}は、{price}円です。</p>
-      <p>現在の{name}は、{price + 222}円です。</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price + 1)}>-1</button>
+      <p>
+        現在の{state.name}は、{state.price}円です。
+      </p>
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <p>
+        現在の{name}は、{price + 222}円です。
+      </p>
+      {/* ここがオブジェクトを取得しないとできない */}
+      {/* ...stateでオブジェクトを展開して、オブジェクトの中のステータスのpriceをかえたいのです。どう変えるの、state.priceを＋１したいのです、と書いてます */}
+      <button onClick={() => setState({ ...state, price: state.price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: state.price - 1 })}>
+        -1
+      </button>
+
+      {/* ここのなんで、１回だけ+されるのかが意味不明 */}
+      <button onClick={() => setState({ ...state, price: price + 10 })}>
+        +10
+      </button>
+      <button onClick={() => setState({ ...state, price: state.price + 10 })}>
+        +10
+      </button>
+
       <button onClick={reset}>Reset</button>
-      <input value={name} onChange={
-        //setNameで状態をとっているので書き換えているだけ。
-        e => setName(e.target.value)
-      }/>
+      <input
+        value={state.name}
+        onChange={
+          //setNameで状態をとっているので書き換えているだけ。
+          //ここが、stateでオブジェクトを展開して、オブジェクトの中の何を変えるの？だから、nameだけでよいとなっています
+          e => setState({ ...state, name: e.target.value })
+        }
+      />
     </>
   );
-
-  
-}
+};
 
 //これでやると外部から引数とる
 //ページをリロードしたらできました。
 App.defaultProps = {
   name: "sample",
   price: 1000
-}
+};
 
 export default App;
