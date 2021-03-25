27720b610fe995b849e56fd82cff34d4da0577b2
nagahama_windows <nagaha1223@gmail.com>
2021-03-25 22:23:13 +0900

    useStateの簡単な使い方

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import React, {useReducer, useState} from "react";
+import React, {createContext, useReducer, useState} from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import reducer from "../reducers/index.js";
 //ここ省略もできて 以下でも同じ意味になるよ
 import reducer from "../reducers";
+import test from "../reducers/test.js";
 
 
 const App = () => {
@@ -12,6 +13,8 @@ const App = () => {
   const [state, dispatch] = useReducer(reducer,[]);
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
+
+  const [ttse, setTtse] = useState(test);
 
   const addEvent = e => {
     //再レンダリングを抑止するためにeの仮引数で、e.preventDefault();としてます。
@@ -28,11 +31,36 @@ const App = () => {
     setBody("");
   }
 
-  console.log({state});
+  
+
+  console.log("~~~~~~~~~~~~~~~");  
+  console.log(ttse.aaa);
+  console.log(ttse.bbb);
+  console.log(ttse);
+  console.log({...test});
+  console.log("####################"); 
+
+  // const aareee = () => {
+  //   setTtse(ttse = "hhhhhhhhhh");
+  // }
+
+  
+  const ae = e => {
+    // useStateの使い方忘れた
+    //再レンダリングを抑止するためにeの仮引数で、e.preventDefault();としてます。
+    e.preventDefault();
+    setTtse(ttse.aaa = "なるほどね");
+    console.log(ttse.bbb = "再レンダリング後");
+    console.log(ttse);
+    console.log({...ttse});
+  }
+
+
 
   return(
     <>
       <div className="container-fluid">
+        <h4>{ttse.aaa}</h4>
         <h4>イベント作成フォーム</h4>
         <form>
           <div className="form-group">
@@ -47,6 +75,7 @@ const App = () => {
 
           <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
           <button className="btn btn-danger">全てのイベントを削除する</button>
+          <button className="btn btn-danger" onClick={ae}>全てのイベントを削除する</button>
           <h4>イベント一覧</h4>
           <table className="table table-hover">
             <thead>
---
+++ src/reducers/test.js
@@ -0,0 +1,30 @@
+// const test = (state = []) => {
+//     console.log(state);
+//     console.log("ドライアド");
+//     const wattt = {
+    //     }
+    // }
+
+import { useState } from "react";
+
+    
+
+//これをエクスポートしてるのか
+// こんなかんじの処理をするのがuseReduseでは?
+// const test = (aa) => {
+//     const arry = {
+//         aaaa: "これはなんだっけ",
+//         bbb: "オブジェクトだっけ"        
+//     }
+//     console.log(arry.bbb + "   ", aa);
+// }
+    
+// useStateは以下のようなオブジェクトを取得するときに使うのがいいかも
+const test = {
+            aaa: "これはなんだっけ",
+            bbb: "オブジェクトだっけ"        
+}
+     
+
+
+export default test;
