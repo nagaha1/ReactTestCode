5976745b6e026839cf53a80b5345d07bb3b5817f
nagahama_windows <nagaha1223@gmail.com>
2021-03-28 19:44:42 +0900

    refactor components

--- src/components/Event.js
+++ src/components/Event.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Event = ({ dispatch, event }) => {
   const id = event.id
   const handleClickDeleteButton = () => {
-    // const result = window.confirm("本当にid="+ id +"の行を削除して良いですか？");
-    const result = window.confirm(`本当にid=${id}`);
+    const result = window.confirm("本当にid="+ id +"の行を削除して良いですか？");
+    // const result = window.confirm(`本当にid=${id}`);
     if(result){dispatch({ type: 'DELETE_EVENT', id })}
   }
 
---
+++ src/components/EventFrom.js
@@ -0,0 +1,61 @@
+//まずはコンポーネントを作成します。
+
+import React,{useState} from "react"
+
+const EventFrom = ({state, dispatch}) => {
+    // const [state, dispatch] = useReducer(reducer, [])
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+   
+    const addEvent = e => {
+        e.preventDefault()
+        
+        dispatch({
+            type: 'CREATE_EVENT',
+            title,
+            body
+        })
+        
+        setTitle('')
+        setBody('')
+    }
+    
+    //レンダリングされていたから、なんか削除されたいたように見えていただけです。ね。
+    const deleteAllEvents = (e) => {
+        e.preventDefault();
+        const result = window.confirm("全てのイベントを本当に削除してもよいですか？");
+        
+        if(result){dispatch({ type: "DELETE_ALL_EVENTS"});}        
+    }
+    
+    const unCreatable = title === "" || body === "";
+    // 下記でもいいけど長い
+    // var unCreatable = true;
+    // if(title === "" || body === ""){
+    //   unCreatable = true;
+    // }else{
+    //   unCreatable = false;
+    // }
+
+    return(
+        <>
+            <h4>イベント作成フォーム</h4>
+            <form>
+            <div className="form-group">
+                <label htmlFor="formEventTitle">タイトル</label>
+                <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}/>
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="formEventBody">ボディー</label>
+                <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}/>
+            </div>
+
+            <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
+            <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.length === 0}>全てのイベントを削除する</button>
+            </form>
+        </>
+    )
+}
+
+export default EventFrom;
